e08778e416ade866082d1857080cf936a353ab3c
Adriaan <adriaan.larmuseau@gmail.com>
2015-11-01 21:55:35 +0100

    setup

--- lib/Compress/Brotli.pm
+++ lib/Compress/Brotli.pm
@@ -1,18 +1,49 @@
 use v6;
 use NativeCall;
 
-module Compress::Brotli:ver<0.1.0>{
+module Compress::Brotli:ver<0.1.0> {
+
+  #======================================
+  # Native functions
+  #======================================
+
+  sub compress_buffer(Config,Int,CArray[uint8], CArray[int], CArray[uint8] --> Int) 
+    is native('libsimplebrotli') { * }
+  sub decompress_buffer(Int,CArray[uint8], CArray[int]--> CArray[uint8]) 
+    is native('libsimplebrotli') { * }
+  sub clear_internal_buffer() is native('libsimplebrotli') { * }
+
+  #======================================
+  # Exceptions
+  #======================================
+
+  class X::Compress::Brotli is Avro::AvroException {
+    has $.message;
+    method message { "Brotli failed: $!message" }
+  }
+
+  #======================================
+  # Config for brotli compression
+  #======================================
+  
+  class Config is repr('CStruct') {
+    has int $.mode;
+    has int $.quality;
+    has int $.lgwin;
+    has int $.lgblock;
+  }
 
   # from Compress::Snappy
 	# Simulate an int pointer with a CArray
-  sub _int_pointer(Int $value = 0) {
+  sub to_pointer(Int $value = 0) {
 	  my $intpointer = CArray[int].new();
 	  $intpointer[0] = $value;
 	  return $intpointer;
   }
 
-  sub compress_buffer(Int,CArray[uint8], CArray[int], CArray[uint8] --> Int) is native('libsimplebrotli') { * }
-  sub decompress_buffer(Int,CArray[uint8], CArray[int], CArray[uint8] --> Int) is native('libsimplebrotli') { * }
+  #======================================
+  # The brotli interface
+  #======================================
 
   our proto compress(Mu --> Buf) is export { * } 
 
@@ -21,16 +52,29 @@ module Compress::Brotli:ver<0.1.0>{
   }
 
   multi sub compress(Blob $data) { 
-
+    # default config
+    my $conf = Config.new(0,11,22,0);
     my $in_size = $data.bytes();
+    my $input = CArray[uint8].new();
+	  $input[$_] = $data[$_] for ^$data.bytes;
     my $max_out_size = 1.2 * $in_size  + 10240;
-
-  
-    compress_buffer($in_size,
+    my $out_size = to_pointer($max_out_size);
+    X::Compress::Brotli.new(:message("Failed to compress!")).throw()
+      unless compress_buffer($conf,$in_size,$input,$out_size,$array);
+	  return Buf.new: map {$array[$_]}, ^$out_size[0];
   }
 
-  sub decompress(Blob --> Buf) is export { * }
-
+  sub decompress(Blob $data --> Buf) is export 
+  { 
+    my $input= CArray[uint8].new();
+	  $input[$_] = $data[$_] for ^$data.bytes;
+    my $array = decompress_buffer($in_size,$input,$out_size);
+    X::Compress::Brotli.new(:message("Failed to decompress")).throw() 
+      unless (^$out_size[0] >= 0);
+	  my $res = Buf.new: map {$array[$_]}, ^$out_size[0];
+    clear_internal_buffer(); #once copied we can clear the buffer
+    return $res;
+  }
 
 }
 
--- stub/brotli.cpp
+++ stub/brotli.cpp
@@ -13,18 +13,85 @@
  * =====================================================================================
  */
 
+#include <vector>
 #include <cstring.h>
+#include <cstdint.h>
 #include <brotli/dec/decode.h>
 #include <brotli/enc/encode.h>
 
+// global variable output, to deal with memory leaks
+// due to perl6 native call being very limited
+std::vector<uint8_t> buffer;
+
+// callback
+int output_callback(void* data, const uint8_t* more, size_t count) 
+{
+  buffer.insert(buffer->end(), more, more + count);
+  return (int)count;
+}
+
+
 extern "C" {
 
- int decompress_buffer(size_t encoded_size,const uint8_t* encoded_buffer,size_t* decoded_size, uint8_t* decoded_buffer)
- {
-  
+  // Brotli compression configuration
+  typedef struct config_s {
+    int mode;
+    int quality;
+    int lgwin;
+    int lgblock;
+  } config;
 
- }
- int compress_buffer(size_t input_size, const uint8_t* input_buffer,size_t* encoded_size, uint8_t* encoded_buffer);
+
+  /* 
+   * ===  FUNCTION  ======================================================================
+   *         Name:  decompress_buffer
+   *  Description:  use the brotli compression function
+   * =====================================================================================
+   */ 
+  uint8_t * decompress_buffer(int encoded_size, const uint8_t* encoded_buffer, size_t* decoded_size);
+  {
+    /* Brotlin Input */
+    BrotliMemInput memin;
+    BrotliInput in = BrotliInitMemInput(encoded_buffer,encoded_size,&memin);
+
+    /* Brotlin Output */
+    BrotliOutput out;
+    out.cb_ = &buffer_callback;
+    buffer.clear();
+    out.data_ = &buffer;
+    
+    if(BrotliDecompress(in, out)){
+      *decoded_size = buffer.size(); 
+      return output.data();
+    } else {
+      *decoded_size = -1;
+      return NULL;
+    }
+  }
+
+  // to limit memory usage
+  void clear_internal_buffer()
+  {
+    buffer.clear(); 
+  }
+
+
+  /* 
+   * ===  FUNCTION  ======================================================================
+   *         Name:  compress_buffer
+   *  Description:  use the brotli buffer compression function
+   * =====================================================================================
+   */ 
+  int compress_buffer(config conf,size_t input_size, const uint8_t* input_buffer,size_t* encoded_size, uint8_t* encoded_buffer);
+  {
+    BrotliParams params;
+    params.mode = conf.mode; 
+    params.quality = conf.quality;
+    params.lgwin = conf.lgwin;
+    params.lgblock = conf.lgblock;
+    return brotli::BrotliCompressBuffer(params,input_size,input_buffer,encoded_size,encoded_buffer);
+  }
+
 
 }
 
