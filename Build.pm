e8694d83e2e25d2f10cf6e9419a8ed6d3b18e709
Ben Davies <kaiepi@outlook.com>
2018-08-04 23:57:19 -0300

    Fix build errors

--- Build.pm
+++ Build.pm
@@ -1,14 +1,23 @@
-use Panda::Common;
-use Panda::Builder;
 use LibraryMake;
 
-class Build is Panda::Builder {
-    method build($workdir) {
-      my Str $os = qx[uname -s 2>/dev/null || echo not];
-      if chomp($os) ~~ "FreeBSD" {
-        shell("cd stub; gmake"); 
-      } else { 
-        shell("cd stub; make"); 
-      }
+class Build {
+    method build($dir) {
+        my %vars = get-vars($dir);
+        if $*VM.osname ~~ 'darwin' | 'freebsd' | 'openbsd' {
+            %vars{'CXX'}      = 'clang++';
+            %vars{'CFLAGS'}   = '-Wall -std=c++11 -lstdc++ -dynamiclib -undefined -suppress -flat_namespace';
+            %vars{'NAME_LIB'} = '-install_name';
+            %vars{'SUFFIX'}   = '.dylib';
+        } else {
+            %vars{'CXX'}      = 'g++';
+            %vars{'CFLAGS'}   = '-Wall -std=c++11 -lstdc++ -shared -fPIC';
+            %vars{'NAME_LIB'} = '-soname';
+            %vars{'SUFFIX'}   = '.so';
+        }
+
+        process-makefile($dir.IO.child('stub').Str, %vars);
+        chdir 'stub';
+        shell(%vars<MAKE>);
+        chdir $*CWD.IO.parent;
     }
 }
